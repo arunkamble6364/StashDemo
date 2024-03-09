39b62b41be76483ab75c9bc662fff5b0b49d36d5
arunkamble6364 <arunkamble6364@gmail.com>
2024-03-10 00:33:48 +0530

    301 code added

--- src/main/java/com/main/SpringBootDemoApplication.java
+++ src/main/java/com/main/SpringBootDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
+	private static final String PAN="Fdtd78";
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 		
