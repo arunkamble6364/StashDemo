5eb41a3616736573ce8ce69b0e683ac6bb5b9c26
arunkamble6364 <arunkamble6364@gmail.com>
2024-03-09 19:11:24 +0530

    Task 2 completed

--- src/main/java/com/main/SpringBootDemoApplication.java
+++ src/main/java/com/main/SpringBootDemoApplication.java
@@ -8,6 +8,10 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		
+			// Task 2
+		System.out.println("modification added");
+		
 	}
 
 }
